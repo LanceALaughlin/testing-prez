ca3a63d5da7d3d4411d7d669a6c0803a0d1aca24
Lance Laughlin <lancealaughlin@gmail.com>
2018-04-05 10:11:42 -0400

    naming crap

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
 import { Container, Menu } from "semantic-ui-react";
 import './App.css';
-import ProductMenu from './components/product-menu'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Crypto $tore</h1>
+          <h1 className="App-title">Secret DVD Marketplace</h1>
         </header>
         <Container className="container">
-          <ProductMenu />
         </Container>
       </div>
     );
